943ef5b8a1c258b8417a753fbc2cdc50940f92e3
dev.ritchie <dev.ritchiek@gmail.com>
2025-04-07 21:41:15 +0300

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,10 +2,7 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
-
 type HomeProps=NativeStackScreenProps<RootStackParamList,'Home'>
-
-
 const Home = ({navigation}:HomeProps) => {
   return (
     <View style={styles.container}>
